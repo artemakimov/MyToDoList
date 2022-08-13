8e213b9498cb0d62f7fbee23eeadb4114ff33bae
artemakimov <86406541+artemakimov@users.noreply.github.com>
2022-08-13 13:50:29 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,50 @@
-var colorArray = [
-  "#cc0404",
-  "#613056",
-  "#05b09c",
-  "#7fff6b",
-  "#6e6a09",
-  "#ff8a05",
-  "#f2b8b8",
-  "#fff",
-];
-var i = 0;
+let colorButtons = document.getElementById('colorButtons');
 
-function changeColor() {
-  var currentColor = colorArray[i];
-  document.body.style.background = currentColor;
-  localStorage.setItem("backgroundColor", currentColor);
-  i++;
-  if (i > colorArray.length - 1) {
-    i = 0;
+colorButtons.addEventListener('click', (e) => {
+  if(e.target.classList.contains("firstColor")){
+    let currentColor = "#0b57a3";
+    document.body.style.backgroundColor = currentColor;
+    localStorage.setItem("backgroundColor", currentColor);
   }
-}
+  if(e.target.classList.contains("secondColor")){
+    let currentColor = "#5e17b6";
+    document.body.style.backgroundColor = currentColor;
+    localStorage.setItem("backgroundColor", currentColor);
+  }
+  if(e.target.classList.contains("thirdColor")){
+    let currentColor = "#1fc042";
+    document.body.style.backgroundColor = currentColor;
+    localStorage.setItem("backgroundColor", currentColor);
+  }
+  if(e.target.classList.contains("fourthСolor")){
+    let currentColor = "#d11b64";
+    document.body.style.backgroundColor = currentColor;
+    localStorage.setItem("backgroundColor", currentColor);
+  }
+  if(e.target.classList.contains("fifthСolor")){
+    let currentColor = "#163e18";
+    document.body.style.backgroundColor = currentColor;
+    localStorage.setItem("backgroundColor", currentColor);
+  }
+  if(e.target.classList.contains("sixthСolor")){
+    let currentColor = "#bde22c";
+    document.body.style.backgroundColor = currentColor;
+    localStorage.setItem("backgroundColor", currentColor);
+  }
+  if(e.target.classList.contains("seventhСolor")){
+    let currentColor = "white";
+    document.body.style.backgroundColor = currentColor;
+    localStorage.setItem("backgroundColor", currentColor);
+  }
+});
+
+window.onload = function () {
+  document.body.style.background = localStorage.getItem("backgroundColor");
+  colorButton.style.backgroundColor = localStorage.getItem("backgroundColorButton");
+};
+let exampleModalLabel = document.getElementById('exampleModalLabel');
+let filterButtons = document.getElementById('filterButtons');
+let divListContent = document.getElementById("divListContent");
 let currentTasks = document.querySelector("#currentTasks");
 let completedTasks = document.getElementById("completedTasks");
 let documentForm = document.querySelector("form");
@@ -42,119 +68,6 @@ window.onload = function () {
   document.body.style.background = localStorage.getItem("backgroundColor");
 };
 
-document.addEventListener("click", (e) => {
-  if (e.target.classList.contains("complButton")) {
-    let taskObject = JSON.parse(
-      localStorage.getItem(e.target.closest("li").id)
-    );
-    taskObject.compl = true;
-    taskAmount -= 1;
-    complTaskAmount += 1;
-    currentTasks.previousElementSibling.innerHTML = `ToDo (${taskAmount})`;
-    completedTasks.previousElementSibling.innerHTML = `Completed (${complTaskAmount})`;
-
-    localStorage.setItem(e.target.closest("li").id, JSON.stringify(taskObject));
-
-    e.target.closest("li").remove();
-    let taskbuffer = createComplTask(taskObject);
-    completedTasks.insertAdjacentHTML("beforeend", taskbuffer);
-  }
-
-  if (e.target.classList.contains("editButton")) {
-    editFlag = true;
-    let task = e.target.closest("li");
-    globTask = task;
-
-    $("#exampleModal").modal("show");
-
-    let taskObject = JSON.parse(localStorage.getItem(task.id));
-
-    inputTitle.value = taskObject.title;
-    inputText.value = taskObject.text;
-    getColor.value = taskObject.color;
-
-    gridRadios.forEach((item) => {
-      if (item.value == taskObject.radio) {
-        item.checked = true;
-      }
-    });
-  }
-
-  if (e.target.classList.contains("delete")) {
-    if (e.target.closest("#completedTasks")) {
-      complTaskAmount -= 1;
-      completedTasks.previousElementSibling.innerHTML = `Completed (${complTaskAmount})`;
-    } else {
-      taskAmount -= 1;
-      currentTasks.previousElementSibling.innerHTML = `ToDo (${taskAmount})`;
-    }
-
-    e.target.closest("li").remove();
-    localStorage.removeItem(e.target.closest("li").id);
-  }
-});
-
-minToMaxButton.addEventListener("click", (event) => {
-  let tasksFromCurr = Array.from(currentTasks.querySelectorAll("li"));
-  let tasksFromCompl = Array.from(completedTasks.querySelectorAll("li"));
-  let todoHTML = "";
-  let completeHTML = "";
-
-  tasksFromCurr.sort(function (a, b) {
-    return a.dataset.fulltime - b.dataset.fulltime;
-  });
-  tasksFromCompl.sort(function (a, b) {
-    return a.dataset.fulltime - b.dataset.fulltime;
-  });
-
-  currentTasks.innerHTML = "";
-  completedTasks.innerHTML = "";
-
-  tasksFromCurr.forEach((item) => {
-    let taskObject = JSON.parse(localStorage.getItem(item.id));
-    todoHTML += createTask(taskObject);
-  });
-
-  tasksFromCompl.forEach((item) => {
-    let taskObject = JSON.parse(localStorage.getItem(item.id));
-    completeHTML += createTask(taskObject);
-  });
-
-  currentTasks.insertAdjacentHTML("beforeend", todoHTML);
-  completedTasks.insertAdjacentHTML("beforeend", completeHTML);
-});
-
-maxToMinButton.addEventListener("click", (event) => {
-  let tasksFromCurr = Array.from(currentTasks.querySelectorAll("li"));
-  let tasksFromCompl = Array.from(completedTasks.querySelectorAll("li"));
-
-  let todoHTML = "";
-  let completeHTML = "";
-
-  tasksFromCurr.sort(function (a, b) {
-    return b.dataset.fulltime - a.dataset.fulltime;
-  });
-  tasksFromCompl.sort(function (a, b) {
-    return b.dataset.fulltime - a.dataset.fulltime;
-  });
-
-  currentTasks.innerHTML = "";
-  completedTasks.innerHTML = "";
-
-  tasksFromCurr.forEach((item) => {
-    let taskObject = JSON.parse(localStorage.getItem(item.id));
-    todoHTML += createTask(taskObject);
-  });
-
-  tasksFromCompl.forEach((item) => {
-    let taskObject = JSON.parse(localStorage.getItem(item.id));
-    completeHTML += createTask(taskObject);
-  });
-
-  currentTasks.insertAdjacentHTML("beforeend", todoHTML);
-  completedTasks.insertAdjacentHTML("beforeend", completeHTML);
-});
-
 documentForm.addEventListener("submit", (ev) => {
   if (editFlag == false) {
     addTask(ev);
@@ -163,31 +76,92 @@ documentForm.addEventListener("submit", (ev) => {
   }
 });
 
+divListContent.addEventListener("click", (e) => {
+  if (e.target.classList.contains("complButton")) {
+    completeTask(e);
+  }
+
+  if (e.target.classList.contains("editButton")) {
+    editTaskGetInfo(e);
+  }
+
+  if (e.target.classList.contains("delete")) {
+    deleteTask(e);
+  }
+});
+
+filterButtons.addEventListener("click", (e) => {
+  if(e.target.classList.contains("minToMaxButton")){
+    tasksFilter(true);
+  }
+  if(e.target.classList.contains("maxToMinButton")){
+    tasksFilter(false);
+  }
+});
+
+function tasksFilter(boolVal){
+  let tasksFromCurr = Array.from(currentTasks.querySelectorAll("li"));
+  let tasksFromCompl = Array.from(completedTasks.querySelectorAll("li"));
+  let todoHTML = "";
+  let completeHTML = "";
+
+  if(boolVal){
+    tasksFromCurr.sort(function (a, b) {
+      return a.dataset.fulltime - b.dataset.fulltime;
+    });
+    tasksFromCompl.sort(function (a, b) {
+      return a.dataset.fulltime - b.dataset.fulltime;
+    });
+  }else{
+    tasksFromCurr.sort(function (a, b) {
+      return b.dataset.fulltime - a.dataset.fulltime;
+    });
+    tasksFromCompl.sort(function (a, b) {
+      return b.dataset.fulltime - a.dataset.fulltime;
+    });
+  }
+
+  currentTasks.innerHTML = "";
+  completedTasks.innerHTML = "";
+
+  tasksFromCurr.forEach((item) => {
+    let taskObject = JSON.parse(localStorage.getItem(item.id));
+    todoHTML += createTask(taskObject, true);
+  });
+
+  tasksFromCompl.forEach((item) => {
+    let taskObject = JSON.parse(localStorage.getItem(item.id));
+    completeHTML += createTask(taskObject, false);
+  });
+
+  currentTasks.insertAdjacentHTML("beforeend", todoHTML);
+  completedTasks.insertAdjacentHTML("beforeend", completeHTML);
+}
+
+
 function addTask(ev) {
   ev.preventDefault();
 
-  let title = inputTitle.value;
-  let text = inputText.value;
   let today = new Date();
   let date =
     today.toLocaleTimeString().slice(0, -3) + " " + today.toLocaleDateString();
   let bigTime = today.getTime();
-  let taskId = generateId();
+
   let radio;
   for (let i = 0; i < gridRadios.length; i++) {
     if (gridRadios[i].checked) {
       radio = gridRadios[i].value;
     }
   }
-  color = getColor.value;
+  let taskId = generateId();
   let taskObject = {
     bigTime: bigTime,
-    title: title,
-    text: text,
+    title: inputTitle.value,
+    text: inputText.value,
     date: date,
     taskId: taskId,
     radio: radio,
-    color: color,
+    color: getColor.value,
     compl: false,
   };
 
@@ -196,16 +170,41 @@ function addTask(ev) {
 
   localStorage.setItem(taskId, JSON.stringify(taskObject));
 
-  let taskbuffer = createTask(taskObject);
+  let taskbuffer = createTask(taskObject, true);
   currentTasks.insertAdjacentHTML("beforeend", taskbuffer);
 
   documentForm.reset();
   $("#exampleModal").modal("hide");
 }
 
-function createTask(taskObjects) {
+function createTask(taskObjects, boolValue) {
   let { bigTime, title, text, date, taskId, radio, color } = taskObjects;
-  let taskHTML = `<li class="list-group-item d-flex w-100 mb-2" data-fullTime="${bigTime}" id="${taskId}" style='background-color:${color}'>
+  let taskHTML
+  if(boolValue){
+        taskHTML = `<li class="list-group-item d-flex w-100 mb-2" data-fullTime="${bigTime}" id="${taskId}" style='background-color:${color}'>
+        <div class="w-100 mr-2">
+            <div class="d-flex w-100 justify-content-between">
+                <h5 class="mb-1">${title}</h5>
+                <div>
+                    <small class="mr-2">${radio} priority</small>
+                    <small class="time">${date}</small>
+                </div>
+            </div>
+            <p class="mb-1 w-100 todoText">${text}</p>
+        </div>
+        <div class="dropdown m-2 dropleft">
+            <button class="btn btn-secondary h-100" type="button" id="dropdownMenuItem1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <i class="fas fa-ellipsis-v"></i>
+            </button>
+            <div class="dropdown-menu p-2 flex-column" aria-labelledby="dropdownMenuItem1" id="dropdown-menu">
+                <button type="button" class="btn btn-success w-100 complButton">Complete</button>
+                <button type="button" class="btn btn-info w-100 my-2 editButton">Edit</button>
+                <button type="button" class="btn btn-danger w-100 delete">Delete</button>
+            </div>
+        </div>
+    </li>`;
+  }else{
+    taskHTML = `<li class="list-group-item d-flex w-100 mb-2" data-fullTime="${bigTime}" id="${taskId}" style='background-color:${color}'>
     <div class="w-100 mr-2">
         <div class="d-flex w-100 justify-content-between">
             <h5 class="mb-1">${title}</h5>
@@ -220,42 +219,17 @@ function createTask(taskObjects) {
         <button class="btn btn-secondary h-100" type="button" id="dropdownMenuItem1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             <i class="fas fa-ellipsis-v"></i>
         </button>
-        <div class="dropdown-menu p-2 flex-column" aria-labelledby="dropdownMenuItem1">
-            <button type="button" class="btn btn-success w-100 complButton">Complete</button>
-            <button type="button" class="btn btn-info w-100 my-2 editButton">Edit</button>
+        <div class="dropdown-menu p-2 flex-column" aria-labelledby="dropdownMenuItem1" id="dropdown-menu">
             <button type="button" class="btn btn-danger w-100 delete">Delete</button>
         </div>
     </div>
 </li>`;
+  }
+  
 
   return taskHTML;
 }
 
-function createComplTask(taskObjects) {
-  let { bigTime, title, text, date, taskId, radio, color } = taskObjects;
-  let taskHTML = `<li class="list-group-item d-flex w-100 mb-2" data-fullTime="${bigTime}" id="${taskId}" style='background-color:${color}'>
-    <div class="w-100 mr-2">
-        <div class="d-flex w-100 justify-content-between">
-            <h5 class="mb-1">${title}</h5>
-            <div>
-                <small class="mr-2">${radio} priority</small>
-                <small class="time">${date}</small>
-            </div>
-        </div>
-        <p class="mb-1 w-100 todoText">${text}</p>
-    </div>
-    <div class="dropdown m-2 dropleft">
-        <button class="btn btn-secondary h-100" type="button" id="dropdownMenuItem1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <i class="fas fa-ellipsis-v"></i>
-        </button>
-        <div class="dropdown-menu p-2 flex-column" aria-labelledby="dropdownMenuItem1">
-            <button type="button" class="btn btn-danger w-100 delete">Delete</button>
-        </div>
-    </div>
-</li>`;
-
-  return taskHTML;
-}
 
 function editioningTask(ev) {
   ev.preventDefault();
@@ -307,6 +281,8 @@ function editioningTask(ev) {
 
   documentForm.reset();
   $("#exampleModal").modal("hide");
+  exampleModalLabel.textContent = "Add Task"
+  submitButton.textContent = 'Add Task'
   editFlag = false;
 }
 
@@ -317,9 +293,9 @@ function generateId() {
 function tasksAmount() {
   taskAmount = currentTasks.getElementsByTagName("li").length;
   complTaskAmount = completedTasks.getElementsByTagName("li").length;
-  currentTasks.previousElementSibling.innerHTML += `(${taskAmount})`;
+  currentTasks.previousElementSibling.innerHTML += ` (${taskAmount})`;
   completedTasks.previousElementSibling.innerHTML +=
-  `(${complTaskAmount})`;
+  ` (${complTaskAmount})`;
 }
 
 function showTasks() {
@@ -334,9 +310,9 @@ function showTasks() {
       let taskObject = JSON.parse(task);
 
       if (taskObject.compl) {
-        completedTasksHTML += createComplTask(taskObject);
+        completedTasksHTML += createTask(taskObject, false);
       } else {
-        currentTasksHTML += createTask(taskObject);
+        currentTasksHTML += createTask(taskObject, true);
       }
     }
   });
@@ -344,31 +320,57 @@ function showTasks() {
   currentTasks.insertAdjacentHTML("beforeend", currentTasksHTML);
   completedTasks.insertAdjacentHTML("beforeend", completedTasksHTML);
 
-  let tasksFromCurr = Array.from(currentTasks.querySelectorAll("li"));
-  let tasksFromCompl = Array.from(completedTasks.querySelectorAll("li"));
-  let todoHTML = "";
-  let completeHTML = "";
+  tasksFilter(true);
+}
 
-  tasksFromCurr.sort(function (a, b) {
-    return a.dataset.fulltime - b.dataset.fulltime;
-  });
-  tasksFromCompl.sort(function (a, b) {
-    return a.dataset.fulltime - b.dataset.fulltime;
-  });
+function deleteTask(ev){
+  if (ev.target.closest("#completedTasks")) {
+    complTaskAmount -= 1;
+    completedTasks.previousElementSibling.innerHTML = `Completed (${complTaskAmount})`;
+  } else {
+    taskAmount -= 1;
+    currentTasks.previousElementSibling.innerHTML = `ToDo (${taskAmount})`;
+  }
 
-  currentTasks.innerHTML = "";
-  completedTasks.innerHTML = "";
+  ev.target.closest("li").remove();
+  localStorage.removeItem(ev.target.closest("li").id);
+}
 
-  tasksFromCurr.forEach((item) => {
-    let taskObject = JSON.parse(localStorage.getItem(item.id));
-    todoHTML += createTask(taskObject);
-  });
+function editTaskGetInfo(ev){
+  editFlag = true;
+    let task = ev.target.closest("li");
+    globTask = task;
+    exampleModalLabel.textContent = "Edit Task"
+    submitButton.textContent = 'Edit Task'
 
-  tasksFromCompl.forEach((item) => {
-    let taskObject = JSON.parse(localStorage.getItem(item.id));
-    completeHTML += createTask(taskObject);
-  });
+    $("#exampleModal").modal("show");
 
-  currentTasks.insertAdjacentHTML("beforeend", todoHTML);
-  completedTasks.insertAdjacentHTML("beforeend", completeHTML);
+    let taskObject = JSON.parse(localStorage.getItem(task.id));
+
+    inputTitle.value = taskObject.title;
+    inputText.value = taskObject.text;
+    getColor.value = taskObject.color;
+
+    gridRadios.forEach((item) => {
+      if (item.value == taskObject.radio) {
+        item.checked = true;
+      }
+    });
+}
+
+function completeTask(e){
+  let taskObject = JSON.parse(
+    localStorage.getItem(e.target.closest("li").id)
+  );
+  taskObject.compl = true;
+  taskAmount -= 1;
+  complTaskAmount += 1;
+  currentTasks.previousElementSibling.innerHTML = `ToDo (${taskAmount})`;
+  completedTasks.previousElementSibling.innerHTML = `Completed (${complTaskAmount})`;
+
+  localStorage.setItem(e.target.closest("li").id, JSON.stringify(taskObject));
+
+  e.target.closest("li").remove();
+  let taskbuffer = createTask(taskObject, false);
+  completedTasks.insertAdjacentHTML("beforeend", taskbuffer);
 }
